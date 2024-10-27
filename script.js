b77bc33bebe7d4e2c3d9eac77ad23fde4a0f366e
dbnexuss <dbnexusss@gmail.com>
2024-10-28 03:28:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// JavaScript for 3D hover effects on nav links
+// JavaScript for 3D hover effects on nav links (Optional)
 document.addEventListener("DOMContentLoaded", () => {
   const navLinks = document.querySelectorAll('.nav-links li a');
 
